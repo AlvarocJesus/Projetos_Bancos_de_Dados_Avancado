23d9d2970857b01724bd2f2a7f7ae61333e48e30
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-04-21 11:23:41 -0300

    Adiciona variaveis para conexao, inicia teste de retornar dados do sql para inserir no mongodb

--- projeto2_documentStore/script.py
+++ projeto2_documentStore/script.py
@@ -5,8 +5,12 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from bson.objectid import ObjectId
 
+# username = 'professor'
+# password = 'FgEp9W9qKAuaAodE'
 
-uri = "mongodb+srv://alvimcoelhojesus:Wz5wyAvFghP6DCPs@projeto2documentstore.bv2jjzy.mongodb.net/?retryWrites=true&w=majority&appName=Projeto2DocumentStore"
+username = 'alvimcoelhojesus'
+password = 'Wz5wyAvFghP6DCPs'
+uri = f"mongodb+srv://{username}:{password}@projeto2documentstore.bv2jjzy.mongodb.net/?retryWrites=true&w=majority&appName=Projeto2DocumentStore"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
@@ -45,38 +49,43 @@ def getDataSQLDB(query):
 # 1. Listar todos os cursos oferecidos por um determinado departamento
 def questao1():
     try:
-        # busca cursos
+        # busca no sql e insere no mongodb os cursos - OK
         cursos = getDataSQLDB('select * from course;')
-        db.course.insert_many(cursos)
-        # busca cursos
-        departamentos = getDataSQLDB("""
+        print('Cursos SQL')
+        print(cursos)
+        teste = db.course.insert_many(cursos)
+        print('Cursos MongoDB')
+        print(teste)
+        print(f'inserted_ids: {teste.inserted_ids}')
+        # busca no sql e insere no mongodb os departamentos
+        """ departamentos = getDataSQLDB(
             select * from department d 
             inner join course c on d.dept_name = c.dept_name
             order by d.dept_name;
-        """)
+        )
 
         departamentosFormatado = []
         for departamento in range(len(departamentos)):
             print(departamentos[departamento])
 
-            """ if departamentos[departamento] == cursos[departamento].dept_name:
+            if departamentos[departamento] == cursos[departamento].dept_name:
                 departamentosFormatado.append({
                     "dept_name": departamentos[departamento][0],
                     "building": departamentos[departamento][1],
                     "budget": departamentos[departamento][2],
                     "courses": [cursos[departamento]._id]
-                }) """
+                })
 
-        # db.course.insert_many(departamentosFormatado)
+        # db.course.insert_many(departamentosFormatado) """
 
 
 
-        departamento = db.department.find({}, { "_id": 0, "name": 1, "courses": 2 })
-        for depart in departamento:
-            for cursoId in depart['courses']:
-                cursos = db.course.find({ "_id": ObjectId(cursoId) }, { "_id": 0, "course_id": 1, "title": 2  })
-                for curso in cursos:
-                    print(f'\nDepartamento: {depart["name"]}\tID do curso: {curso["course_id"]}\tNome do curso: {curso["title"]}')
+        # departamento = db.department.find({}, { "_id": 0, "name": 1, "courses": 2 })
+        # for depart in departamento:
+        #     for cursoId in depart['courses']:
+        #         cursos = db.course.find({ "_id": ObjectId(cursoId) }, { "_id": 0, "course_id": 1, "title": 2  })
+        #         for curso in cursos:
+        #             print(f'Departamento: {depart["name"]}\tID do curso: {curso["course_id"]}\tNome do curso: {curso["title"]}')
     except Exception as e:
         print(f"Deu errado {e}")
 

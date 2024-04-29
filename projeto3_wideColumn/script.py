b9bc677016e06ec04987c6dafa336dfa918708fb
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-04-29 18:08:48 -0300

    Termina de configurar projeto 3 e resolve questao 1

--- projeto3_wideColumn/script.py
+++ projeto3_wideColumn/script.py
@@ -1,14 +1,107 @@
-from astrapy import DataAPIClient
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 from configparser import ConfigParser
+from time import sleep
 
 config = ConfigParser().read('../config.ini')
 # print(config.get("ASTRADB","astraURL"))
 
+""" Teste 1
+from astrapy import DataAPIClient
 # Initialize the client
-client = DataAPIClient(config["ASTRADB"]["astraToken"])
+client = DataAPIClient('AstraCS:ndBfjDoOdoTuCUTQdTppRzbZ:5620e5c809dd904b5bfadc498e4afd6ecd3cc417eaa64f1d8eddbe6032bf52e6')
 db = client.get_database_by_api_endpoint(
-  config["ASTRADB"]["astraURL"],
+  'https://249f483f-685e-4b8a-8436-ae34dd6f7103-us-east-2.apps.astra.datastax.com'
 )
 
-print(f"Connected to Astra DB: {db.list_collection_names()}")
+print(f"Connected to Astra DB: {db.list_collection_names()}") """
 
+# Teste 2
+import os
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+session = Cluster(
+    cloud={
+			"secure_connect_bundle": SECURECONNECTBUNDLEPATH,
+			'connect_timeout': 30
+		},
+    auth_provider=PlainTextAuthProvider("token", astraToken),
+).connect()
+
+print(f'Session: {session}')
+
+# Cria todas as tabelas no banco Cassandra
+"""
+- Questao 1:
+CREATE TABLE default_keyspace.course_department (
+    dept_name text,
+    building text,
+    budget text,
+    course_id text,
+		title text,
+		credits text,
+    PRIMARY KEY (dept_name, course_id, title)
+);
+"""
+
+# Realiza o select no banco de dados SQL
+def getDataSQLDB(query):
+	try:
+		# Cria uma conexão com o banco de dados
+		engine = create_engine('postgresql://wpwvldie:FZJVGeEX5HWudTq769cmen4Ytxr-ixxL@silly.db.elephantsql.com/wpwvldie', echo=True)
+		Session = sessionmaker(bind=engine)
+		session = Session()
+		
+		# Cria um cursor para executar as queries
+		conn = engine.connect()
+		
+		# Executa a query
+		data = conn.execute(text(query)).all()
+		
+		# Fecha a conexão
+		session.close()
+		
+		return data
+	except Exception as e:
+		print(f"Deu errado {e}")
+
+def deleteDataCassandraDB(collection):
+	try:
+		session.execute(f'truncate defaul_keyspace.{collection}')
+	except Exception as e:
+		print(f"Deu errado {e}")
+
+# 1. Listar todos os cursos oferecidos por um determinado departamento
+def questao1():
+  try:
+    print("Questão 1")
+		
+    # Deleta os dados da tabela
+    deleteDataCassandraDB('course_department')
+		
+    cursos = getDataSQLDB("""
+      select 
+        *
+      from department d 
+      inner join course c on d.dept_name = c.dept_name;
+    """)
+
+    cursosCassandra = []
+		
+    for curso in cursos:
+      query = f"INSERT INTO default_keyspace.course_department(dept_name, building, budget, course_id, title, credits) VALUES ('{curso[0]}', '{curso[1]}', '{curso[2]}', '{curso[3]}', '{curso[4]}', '{curso[6]}');"
+      addCurso = session.execute(query)
+      cursosCassandra.append(addCurso)
+      sleep(1)
+      		
+    # Resolucao
+    departamentosCursos = session.execute('select * from default_keyspace.course_department;')
+    print('----------Cursos CASSANDRA----------')
+    for curso in departamentosCursos:
+      print(curso)
+
+  except Exception as e:
+    print(f"Deu errado {e}")
+
+questao1()

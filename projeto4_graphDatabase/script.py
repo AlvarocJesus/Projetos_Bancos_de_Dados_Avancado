f3df17155501ab3573eef603e7f2af978042a8a0
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-05-18 21:50:34 -0300

    Adiciona resolucao das questoes no neo4j

--- projeto4_graphDatabase/script.py
+++ projeto4_graphDatabase/script.py
@@ -39,30 +39,116 @@ def deleteDataMongoDB(collection):
 	except Exception as e:
 		print(f"Deu errado {e}")
 
-
 # 1. Listar todos os cursos oferecidos por um determinado departamento
-def questao1(driver):
+def questao1(driver, dept_name):
   
-	teste = getDataSQLDB("select * from teaches;")
-	print(teste)
+	teste = getDataSQLDB("select * from course;")
   
   # Cria a query
-	for adv in teste:
-		query = "match (i:instructor { id: '" + adv[0] + "' })\n"
-		query += "(s:section { course_id: '"+ adv[1] + "', sec_id: '"+ adv[2] + "', semester: '"+ adv[3] + "', year: "+ str(adv[4]) + " })\n"
-		query += "create (i)-[:TEACHES]->(s);"
-		print(query)
-		# result = session.run(query)
-		# result = driver.execute_query(query)
-		# print(f'Result: {result}')
-		sleep(1)
+	query = "MATCH (d:department { dept_name: '" + dept_name + "' }) WITH d MATCH (c:course) RETURN d, c;"
+	# result = session.run(query)
+	result, summary, keys = driver.execute_query(query)
 
+	# Exibe o resultado
+	for item in result:
+		# print(item.data())
+		print(f"Departamento: {item['d']['dept_name']} tem o curso {item['c']['course_id']} - {item['c']['title']}")
+			
 # 2. Recuperar todas as disciplinas de um curso específico em um determinado semestre
+def questao2(driver):
+  
+	teste = getDataSQLDB("select * from course;")
+  
+  # Cria a query
+	query = 'MATCH p=(course { title: "Computational Biology" })-[:section {semester: "Summer"}]->() RETURN p;'
+	# result = session.run(query)
+	result, summary, keys = driver.execute_query(query)
+
+	# Exibe o resultado
+	for item in result:
+		print(item.data())
+		# print(f"course_id: {item['p']['course_id']}, credits: {item['p']['credits']}, dept_name: {item['p']['dept_name']}, title: {item['p']['title']}, room_number: {item['section']['room_number']}, building: {item['section']['building']}, capacity: {item['section']['capacity']}")
 
 # 3. Encontrar todos os estudantes que estão matriculados em um curso específico
+def questao3(driver):
+  
+	teste = getDataSQLDB("select * from course;")
+  
+  # Cria a query
+	query = "MATCH (c:course) with c MATCH (s:student) return s, c;"
+	# result = session.run(query)
+	result, summary, keys = driver.execute_query(query)
+
+	# Exibe o resultado
+	for item in result:
+		# print(item.data())
+		print(f"O aluno {item['s']['name']} está matriculado no curso {item['c']['course_id']} - {item['c']['title']}")
+
+# Questão 4: Listar a média de salários de todos os professores em um determinado departamento
+def questao4(department_name, driver):
+	# Cria a query
+	query = f"MATCH (s:instructor) WHERE s.dept_name = '{department_name}' RETURN s.salary;"
+
+	# Executa a query
+	result, summary, keys = driver.execute_query(query)
+
+	cout = 0 
+	soma = 0
+	for record in result:
+		cout += 1
+		soma += int(record['s.salary'])
+
+	print(f"A média de salários dos professores do departamento {department_name} é de R${soma/cout:.2f}\n")
+
+#Questão 5: Recuperar o número total de créditos obtidos por um estudante específico pelo nome
+def questao5(student_name, driver):
+	# Cria a query
+	query = f"MATCH (s:student) WHERE s.name = '{student_name}' RETURN s;"
+
+	# Executa a query
+	result, summary, keys = driver.execute_query(query)
+
+
+	# Imprime o resultado
+	for record in result:
+		print(record.data())
+		print(f"O estudante {record['s']['name']} possui um total de {record['s']['tot_cred']} créditos\n")
+
+#Questão 6: Encontrar todas as disciplinas ministradas por um professor em um semestre específico
+def questao6(instructor_name, semester, driver):
+	# Cria a query
+	query = "MATCH p=({name: '"+instructor_name+"'})-[:teaches{semester: '"+semester+"'}]->() RETURN p;"
+
+	# Executa a query
+	result, summary, keys = driver.execute_query(query)
+
+
+	# Imprime o resultado
+	for record in result:
+		print(record.data())
+		# print(f"O professor {instructor_name} ministrou as seguintes disciplinas no semestre {semester}:")
 
 # 10. Recuperar a quantidade de alunos orientados por cada professor
+def questao10(driver, instructor):
+  
+	teste = getDataSQLDB("select * from course;")
+  
+  # Cria a query
+	query = "MATCH p=({ name: '" + instructor + "' })-[:ADVISOR]->() RETURN p;"
+	# result = session.run(query)
+	result, summary, keys = driver.execute_query(query)
+
+	# Exibe o resultado
+	for item in result:
+		print(item.data())
+		# print(f"Departamento: {item['d']['dept_name']} tem o curso {item['c']['course_id']} - {item['c']['title']}")
 
 with GraphDatabase.driver("neo4j+s://a38c18e1.databases.neo4j.io", auth=("neo4j","89cyD-kbrtzfBaJzrxTTy3hALKSDXxT2zxvHkyquzuQ")) as driver:
 		driver.verify_connectivity()
-		questao1(driver)
+		# questao1(driver, 'Comp. Sci.')
+		# questao2(driver)
+		# questao3(driver)
+		#questao4("Finance", driver)
+		#questao5("Zhang", driver)
+		questao6("Zhang", "Fall", driver)
+		# questao10(driver, 'Einstein')
